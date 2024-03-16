8b7f087347fed6a96276bd916dfcf1df92723610
Rafael Kimura <rafaelkimura57@gmail.com>
2024-03-15 22:07:55 -0300

    Publicando projeto

---
+++ src/main/java/com/kimura/api/TravelApi/TravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.kimura.api.TravelApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelApiApplication.class, args);
+	}
+
+}
